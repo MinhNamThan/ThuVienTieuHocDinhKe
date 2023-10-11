f742277361e55327508e2cd8b6cac7aeb0227fed
MinhNamThan <thanminhnam17022001@gmail.com>
2023-10-11 18:53:44 +0700

    fix email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         redirect_to edit_user_path(@user)
       else
         flash[:alert] = "Không thành công"
-        render :new
+        redirect_to edit_user_path(@user)
       end
   end
 
